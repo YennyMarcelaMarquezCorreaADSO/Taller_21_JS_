e243a88e0579174d3a5e0afdd7cb4c9c256fc97d
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 16:56:59 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución, por favor utilizar funciones matemáticas de Python para ello
-    //d = rc((x2-x1)^2 + (y2-y1)^2)
+    // Ejercicio 22. Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total de la compra.
 
-    let rc = Number(prompt("Ingrese un valor: "))
-    let x1 = Number(prompt("Ingrese el valor: "))
-    let x2 = Number(prompt("Ingrese el valor: "))
-    let y1 = Number(prompt("Ingrese el valor: "))
-    let y2 = Number(prompt("Ingrese el valor: "))
+    let producto1 = Number(prompt("Ingrese un valor del producto 1: "))
+    let producto2 = Number(prompt("Ingrese el valor del producto 2: "))
+    let producto3 = Number(prompt("Ingrese el valor del producto 3: "))
+    let producto4 = Number(prompt("Ingrese el valor del producto 4: "))
+    let producto5 = Number(prompt("Ingrese el valor del producto 5: "))
 
-    let solucion = rc*((x2-x1)**2 + (y2-y1)**2)
+    let descuento1 = (producto1*5/100)
+    let descuento2 = (producto2*5/100)
+    let descuento4 = (producto4*2/100)
+    let descuento5 = (producto5*2/100)
 
-    console.group(`%cFormula (${rc}(${x2}-${x1})**${2}+(${y2}-${y1})**${2})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    let valorcondes1 = (producto1 - descuento1)
+    let valorcondes2 = (producto2 - descuento2)
+    let valorcondes4 = (producto4 - descuento4)
+    let valorcondes5 = (producto5 - descuento5)
 
-        console.log(`La solución es: : "${solucion}"`);
+    let total = (valorcondes1 + valorcondes2 + producto3 + valorcondes4 + valorcondes5)
+
+// -------------------------------------------------------------
+    console.group(`%cFormula ${producto1} - ${descuento1}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+    console.log(`Costo producto 1 con descuento: : "${valorcondes1}"`);
     console.groupEnd();
+
+// -----------------------------------------------------------
+
+    console.group(`%cFormula ${producto2} - ${descuento2}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+        console.log(`Costo producto 2 con descuento: "${valorcondes2}"`);
+    console.groupEnd();
+
+// -----------------------------------------------------------
+
+
+    console.log(`Costo del tercer producto: : "${producto3}"`);
+
+// -----------------------------------------------------------
+
+    console.group(`%cFormula ${producto4} - ${descuento4}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+    console.log(`Costo del cuarto producto con descuento: "${valorcondes4}"`);
+    console.groupEnd();
+
+// -----------------------------------------------------------
+
+    console.group(`%cFormula ${producto5} - ${descuento5}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+    console.log(`Costo del quinto producto con descuento: : "${valorcondes5}"`);
+    console.groupEnd();
+
+// -----------------------------------------------------------
+
+    console.group(`%cFormula (${valorcondes1} + ${valorcondes2} + ${producto3} + ${valorcondes4} + ${valorcondes5})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+    console.log(`Valor total: : "${total}"`);
+    console.groupEnd();
+
+// --------------------------------------------------------------
 
 })

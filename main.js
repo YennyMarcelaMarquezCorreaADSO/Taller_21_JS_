cd62084fbd61817240c8b920bc5716ad82ecbe47
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 20:39:30 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 29. 
-//Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,bpero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código que representen el algoritmo para solucionar este problema. Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula sostenida y la edad del entrevistado.
+    // Ejercicio 30. 
+// Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase invertida.
     
 
-    let nombre = prompt("Ingrese su nombre: ")
-    let año = Number(prompt("Ingrese el año actual: "))
-    let añona = Number(prompt("Ingrese su año de nacimiento: "))
+    let frase,invertida
 
-    let edad = año - añona
-   
-    console.group(`%cFormula ${año} - ${añona}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    frase = "Hay dos formas de escribir programas sin errores; sólo la tercera funciona"
 
-    console.log(`edad: "${edad}"`);
-    console.groupEnd();
+    invertida = texto.split("").reverse().join("");
 
-    console.log(`El/La solicitante "${nombre}" tiene una edad de "${edad}" años`);
+    console.log(invertida)
+ 
 })
